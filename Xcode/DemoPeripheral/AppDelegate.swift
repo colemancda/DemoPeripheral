147e2fff11078ca694253f3a820386e8bd971416
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-30 23:10:38 -0500

    Workaround for SR-2227

--- Xcode/DemoPeripheral/AppDelegate.swift
+++ Xcode/DemoPeripheral/AppDelegate.swift
@@ -13,7 +13,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         // print app info
